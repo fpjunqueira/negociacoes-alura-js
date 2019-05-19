423553c2aafc0ec5b824671d4cf6fffd2a4cc6ec
Felipe Junqueira <felipetiuzanata@gmail.com>
2019-05-19 19:58:34 -0300

    Update view with proxy

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,9 +7,55 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade'); 
         this._inputValor = $('#valor');
 
-        this._listNegociacoes = new ListaNegociacoes(
-            model => this._negociacoesView.update(model)
-        );
+        /**
+         * O Proxy do JS só consegue interceptar uma ação de um objeto
+         * quando uma propriedade é lida ou recebe um valor (set/get). 
+         * Propriedades de funções não são interceptadas pelo proxy.
+         * 
+         * Solucação para interceptação de uma função do objeto:
+         * 
+         *  - Comportamento: ao executar uma função o JS primeiro 
+         *    da um 'get' da propriedade que representa a função para, 
+         *    em seguida, executar Reflection.apply().
+         * 
+         * Através desse comportamento é possível incerptar um função.
+         * 
+         *  - sempre que for um método usar Reflect.apply()
+         */
+
+         // guardando o contexto
+        const self = this;
+
+        this._listNegociacoes = new Proxy(new ListaNegociacoes(), {
+
+            get(target, prop, receiver) {
+
+                const isAdicionaOuEsvazia = ['adiciona', 'esvazia'].includes(prop);
+                const isOfTypeFunction = typeof (target[prop]) == typeof (Function);
+                if (isAdicionaOuEsvazia && isOfTypeFunction) {
+
+                    // Substitui o método do proxy que está sendo chamado pelo função retornada
+                    // não pode ser array function, pois precisa ter contexto dinâmico
+                    return function () {
+
+                        console.log(`${prop} intercepted`);
+
+                        // executa função do objeto "proxyado" (adiciona ou esvazia)
+                        // parametros: função a ser executada, contexto de execução, argumentos
+                        // o arguments são os argumentos que adiciona ou esvazia está recebendo
+                        Reflect.apply(target[prop], target, arguments);
+
+                        // update view
+                        self._negociacoesView.update(target);
+                    }
+
+                }
+
+                return Reflect.get(target, prop, receiver);
+            }
+
+        });
+        
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listNegociacoes);
